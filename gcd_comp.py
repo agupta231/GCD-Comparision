06ab28ce174a6e8592ec399480df0fdcb9693675
Ankur Gupta <rukna1000@gmail.com>
2017-10-26 04:05:20 -0400

    Finished CICA

--- gcd_comp.py
+++ gcd_comp.py
@@ -5,3 +5,15 @@ def euclid(m, n):
         n = r
 
     return m
+
+
+def cica(m, n):
+    t = min(m, n)
+
+    while t > 0:
+        if m % t == 0 and n % t == 0:
+            return t
+        else:
+            t -= 1
+
+    return 1
