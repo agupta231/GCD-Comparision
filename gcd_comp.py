11c4a67fd37fd2053d2f549e257670ca5ad63193
Ankur Gupta <rukna1000@gmail.com>
2017-11-08 04:09:10 -0500

    Fixed the middle school algorithm

--- gcd_comp.py
+++ gcd_comp.py
@@ -1,5 +1,4 @@
 import math
-
 import time
 
 
@@ -43,19 +42,34 @@ def middle_school(m, n):
 
         return True
 
-    def prod_list(lst):
-        prod = 1
+    def determine_gcd(m_primes, n_primes):
+        primes_tally = dict()
 
-        for val in lst:
-            prod *= val
+        for m in m_primes:
+            if m in primes_tally:
+                print(m)
 
-        return prod
+                primes_tally[m] = [primes_tally[m][0] + 1, primes_tally[m][1]]
+            else:
+                count = 0
+
+                for n in n_primes:
+                    if n == m:
+                        count += 1
+
+                primes_tally[m] = [1, count]
+
+        gcd = 1
+
+        for prime in primes_tally.keys():
+            gcd *= prime ** min(primes_tally[prime])
+
+        return gcd
 
     m_primes = prime_factors(m)
     n_primes = prime_factors(n)
 
-    common_primes = set(n_primes) & set(m_primes)
-    return prod_list(common_primes)
+    return determine_gcd(m_primes, n_primes)
 
 
 def effGCD(s1, s2):
@@ -71,9 +85,9 @@ def effGCD(s1, s2):
     gcd_ms = middle_school(s1, s2)
     end_time_ms = time.clock()
 
-    print("Euclid        -- GCD Computed: " + str(gcd_euclid) + " CPU time: " + str(end_time_euclid - start_time_euclid))
-    print("CICA          -- GCD Computed: " + str(gcd_cica) + " CPU time: " + str(end_time_cica - start_time_cica))
-    print("Middle School -- GCD Computed: " + str(gcd_ms) + " CPU time: " + str(end_time_ms - start_time_ms))
+    print("Euclid        -- GCD Computed: " + str(gcd_euclid) + " CPU time (seconds): " + str(end_time_euclid - start_time_euclid))
+    print("CICA          -- GCD Computed: " + str(gcd_cica) + " CPU time (seconds): " + str(end_time_cica - start_time_cica))
+    print("Middle School -- GCD Computed: " + str(gcd_ms) + " CPU time (seconds): " + str(end_time_ms - start_time_ms))
 
 
 def UI():
@@ -89,7 +103,7 @@ def UI():
 
             if n == 0 or m == 0:
                 print("0 is an invalid option, try again")
-                raise Exception
+                continue
 
             print("\n")
 
@@ -104,3 +118,54 @@ def UI():
 
 
 UI()
+
+
+# def prime_factors(num):
+#     if is_prime(num):
+#         return [num]
+#
+#     for i in range(2, int(math.sqrt(num)) + 1):
+#         if num % i == 0:
+#             return prime_factors(i) + prime_factors(int(num / i))
+#
+#
+# def is_prime(val):
+#     if val == 2:
+#         return True
+#
+#     for i in range(2, int(math.ceil(math.sqrt(val))) + 1):
+#         if val % i == 0:
+#             return False
+#
+#     return True
+#
+#
+# m_primes = prime_factors(1000)
+# n_primes = prime_factors(10000)
+#
+#
+# def determine_gcd(m_primes, n_primes):
+#     primes_tally = dict()
+#
+#     for m in m_primes:
+#         if m in primes_tally:
+#             print(m)
+#
+#             primes_tally[m] = [primes_tally[m][0] + 1, primes_tally[m][1]]
+#         else:
+#             count = 0
+#
+#             for n in n_primes:
+#                 if n == m:
+#                     count += 1
+#
+#             primes_tally[m] = [1, count]
+#
+#     gcd = 1
+#
+#     for prime in primes_tally.keys():
+#         gcd *= prime ** min(primes_tally[prime])
+#
+#     return gcd
+#
+# print(determine_gcd(m_primes, n_primes))
